c67c859ad6cfb16fa7c3cf4a3ec21a359fab0344
Israa Irshad <israa@qburst.com>
2024-12-02 14:17:06 +0530

    Arrays as response

--- tutorial/main.py
+++ tutorial/main.py
@@ -3,6 +3,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+books = [
+    {'title': 'Atomic Habits', 'author': 'James Clear'},
+    {'title': 'Twisted Love', 'author': 'Ana Huang'},
+    {'title': 'The Midnight Library', 'author': 'Matt Heig'},
+
+]
+
 @app.get('/')
 def welcome_page():
     return {'message': 'Hello World'}
+
+
+@app.get('/books/')
+def list_books():
+    return books

a0830c7b77a063b96185ba7c9d25b4fd1ca167f9
Israa Irshad <israa@qburst.com>
2024-12-03 17:41:38 +0530

    setting optional fields in the payload

--- tutorial/validations.py
+++ tutorial/validations.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class ValidateBook(BaseModel):
-    id: int
+    id: Optional[int] = None
     title: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)

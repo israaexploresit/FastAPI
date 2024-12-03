a56ce970bd200f6cb72b1818c67f17fdda181b59
Israa Irshad <israa@qburst.com>
2024-12-03 17:47:41 +0530

    Schema setting extra description

--- tutorial/validations.py
+++ tutorial/validations.py
@@ -3,7 +3,8 @@ from typing import Optional
 
 
 class ValidateBook(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not a required field',
+                              default=None)
     title: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)

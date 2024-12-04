43419e842a16a26c118d7a7d5dba221dee940a01
Israa Irshad <israa@qburst.com>
2024-12-04 10:07:36 +0530

    Configure Docs/Swagger example

--- tutorial/validations.py
+++ tutorial/validations.py
@@ -8,3 +8,13 @@ class ValidateBook(BaseModel):
     title: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        'json_schema_extra': {
+            "example": {
+                'title': 'Book 1',
+                'description': 'Nice book',
+                'rating': 5
+                }
+        }
+    }

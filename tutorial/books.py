76ee7eb195772b3d8ba2aafbe839b05cc68be081
Israa Irshad <israa@qburst.com>
2024-12-05 11:02:27 +0530

    Set up default status codes for each API using starlette.status

--- tutorial/books.py
+++ tutorial/books.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Path, Query, HTTPException
 from tutorial.models import Book
 from tutorial.validations import ValidateBook
 from tutorial.utils import reform_to_book_class, assign_book_id
+from starlette import status
 
 app = FastAPI()
 
@@ -13,19 +14,19 @@ BOOKS = [
 ]
 
 
-@app.get('/books')
+@app.get('/books', status_code=status.HTTP_200_OK)
 async def get_books():
     return BOOKS
 
 
-@app.post('/books')
+@app.post('/books', status_code=status.HTTP_201_CREATED)
 async def create_book(request_data: ValidateBook):
     request_data = reform_to_book_class(request_data)
     request_data = assign_book_id(request_data, BOOKS)
     BOOKS.append(request_data)
 
 
-@app.get('/books/{book_id}')
+@app.get('/books/{book_id}',status_code=status.HTTP_200_OK)
 async def get_book(book_id: int = Path(gt=0)):
     response = list(filter(lambda x: x.id == book_id, BOOKS))
     if not response:
@@ -33,13 +34,13 @@ async def get_book(book_id: int = Path(gt=0)):
     return response
 
 
-@app.get('/books/')
+@app.get('/books/', status_code=status.HTTP_200_OK)
 async def fetch_books_by_rating(rating: int = Query(gt=0, lt=6)):
     response = list(filter(lambda x: x.rating == rating, BOOKS))
     return response
 
 
-@app.put('/books/')
+@app.put('/books/', status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(request_data: ValidateBook):
     is_book_updated = False
     for i in range(len(BOOKS)):
@@ -50,7 +51,7 @@ async def update_book(request_data: ValidateBook):
         raise HTTPException(status_code=404, detail='Book not found')
 
 
-@app.delete('/books/{book_id}')
+@app.delete('/books/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int = Path(gt=0)):
     is_book_deleted = False
     for i in range(len(BOOKS)):

c11c0ef43592694726c98488d3c17d453f7bee08
Israa Irshad <israa@qburst.com>
2024-12-06 11:51:32 +0530

    GET single entity Request with ORM

--- tutorial/ToDosApp/main.py
+++ tutorial/ToDosApp/main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 
 from database import engine, SessionLocal
 from models import ToDo
@@ -22,6 +23,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get('/books')
+@app.get('/todos')
 async def list_books(db: db_dependency):
     return db.query(ToDo).all()
+
+
+@app.get('/todos/{todo_id}', status_code=status.HTTP_200_OK)
+async def get_book(db: db_dependency, todo_id: int = Path(gt=0)):
+    response = db.query(ToDo).filter(ToDo.id == todo_id).first()
+    if response:
+        return response
+    raise HTTPException(status_code=404, detail='ToDo not found')

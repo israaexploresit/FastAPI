ccf319f1a9e4d71a16dbfbe970f65cf0a67ea561
Israa Irshad <israa@qburst.com>
2024-12-06 15:41:43 +0530

    PUT : update todo- ORM

--- tutorial/ToDosApp/main.py
+++ tutorial/ToDosApp/main.py
@@ -24,7 +24,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-class CreateToDo(BaseModel):
+class ToDoData(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=3, max_length=100)
     priority: int = Field(gt=0)
@@ -45,7 +45,22 @@ async def get_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @app.post('/todos', status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency, request_data: CreateToDo):
+async def create_todo(db: db_dependency, request_data: ToDoData):
     todo_obj = ToDo(**request_data.model_dump())
+    db.add(todo_obj)
+    db.commit()
+
+
+@app.put('/todos/{todo_id}/', status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency,
+                      request_data: ToDoData,
+                      todo_id: int = Path(gt=0)):
+    todo_obj = db.query(ToDo).filter(ToDo.id == todo_id).first()
+    if not todo_obj:
+        raise HTTPException(status_code=404, detail='ToDo not found')
+    todo_obj.title = request_data.title
+    todo_obj.description = request_data.description
+    todo_obj.priority = request_data.priority
+    todo_obj.is_completed = request_data.is_completed
     db.add(todo_obj)
     db.commit()

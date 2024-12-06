1c1e89ca370411bbb5e8651b210a719fb4ebd0e8
Israa Irshad <israa@qburst.com>
2024-12-06 11:35:50 +0530

    GET Request with ORM

--- tutorial/ToDosApp/main.py
+++ tutorial/ToDosApp/main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
-from database import engine
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+
+from database import engine, SessionLocal
+from models import ToDo
 import models
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get('/books')
+async def list_books(db: db_dependency):
+    return db.query(ToDo).all()

3f5081d9647585fe911b067acbb8e7282980f5b1
Israa Irshad <israa@qburst.com>
2024-12-09 18:24:06 +0530

    Enhanced a db by adding new table 'Users'

--- tutorial/ToDosApp/models.py
+++ tutorial/ToDosApp/models.py
@@ -2,8 +2,21 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
 class ToDo(Base):
-    __tablename__ = 'todo'
+    __tablename__ = 'todos'
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
